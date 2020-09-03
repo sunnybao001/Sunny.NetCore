f659ba7159c728a0c1234773b78399720b074219
思宁 <sunny.bao@qq.com>
2020-09-03 11:35:19 +0800

    Aggressive Inlining

--- Sunny.NetCore.Extension/Converter/MvcInterface.cs
+++ Sunny.NetCore.Extension/Converter/MvcInterface.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,7 @@ namespace Sunny.NetCore.Extension.Converter
 				CallingConventions.Standard | CallingConventions.HasThis,
 				contextReturn,
 				new Type[] { contextParam });
+			method.SetCustomAttribute(new CustomAttributeBuilder(typeof(MethodImplAttribute).GetConstructor(new Type[] { typeof(MethodImplOptions) }), new object[] { MethodImplOptions.AggressiveInlining }));
 			var il = method.GetILGenerator();
 			var loc0 = il.DeclareLocal(typeof(Guid));
 			il.Emit(OpCodes.Ldarg_1);
@@ -83,6 +85,7 @@ namespace Sunny.NetCore.Extension.Converter
 				CallingConventions.Standard | CallingConventions.HasThis,
 				typeof(Task),
 				new Type[] { bindingParam });
+			method.SetCustomAttribute(new CustomAttributeBuilder(typeof(MethodImplAttribute).GetConstructor(new Type[] { typeof(MethodImplOptions) }), new object[] { MethodImplOptions.AggressiveInlining }));
 			var il = method.GetILGenerator();
 			var loc0 = il.DeclareLocal(typeof(string));
 			var loc1 = il.DeclareLocal(typeof(TR));
